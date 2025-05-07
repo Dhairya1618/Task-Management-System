8ca23379426283b25b34a5d58a6b91a9db500109
dhairya-wotsabot <dhairya.brijesh@wotsabot.com>
2025-05-07 21:36:47 +0530

    any message

--- login.php
+++ login.php
@@ -1,18 +1,12 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Login</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+<head><title>Login</title></head>
 <body>
-    <div class="login-container">
-        <h2>Login</h2>
-        <form action="backend/login.php" method="POST" class="login-form">
-            <input type="text" name="username" placeholder="Username" required><br>
-            <input type="password" name="password" placeholder="Password" required><br>
-            <input type="submit" value="Login">
-        </form>
-        <p>Don't have an account? <a href="register.php">Register here</a></p>
-    </div>
+  <h2>Login</h2>
+  <form action="backend/login.php" method="POST">
+    <input type="text" name="username" placeholder="Username" required><br><br>
+    <input type="password" name="password" placeholder="Password" required><br><br>
+    <input type="submit" value="Login">
+  </form>
 </body>
 </html>
--- register.php
+++ register.php
@@ -2,17 +2,20 @@
 <html>
 <head>
     <title>Register</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css"> <!-- Update path if needed -->
 </head>
 <body>
     <div class="login-container">
         <h2>Register</h2>
-        <form action="backend/register.php" method="POST" class="login-form">
+        <form action="register.php" method="POST" class="login-form">
             <input type="text" name="username" placeholder="Username" required><br>
             <input type="password" name="password" placeholder="Password" required><br>
             <input type="submit" value="Register">
         </form>
-        <p>Already have an account? <a href="login.php">Login here</a></p>
+        <p style="margin-top: 10px;">
+            Already have an account? <a href="login.php">Login</a>
+          </p>
+
     </div>
 </body>
 </html>
